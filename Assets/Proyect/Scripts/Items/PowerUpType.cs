9cf6919d7a11f133639f2975e097e7e9448f408a
Sofia Vanegas Cordoba <sofia.vanegasc@udea.edu.co>
2026-07-09 16:10:01 -0500

    Cambio en las Animaciones del Jugador

---
+++ Assets/Proyect/Scripts/Items/PowerUpType.cs
@@ -0,0 +1,6 @@
+public enum PowerUpType
+{
+    VelocityBoost,
+    AttackBoost,
+    DefenseBoost
+}
--- Assets/Proyect/Scripts/Player/PlayerActions.cs
+++ Assets/Proyect/Scripts/Player/PlayerActions.cs
@@ -1,18 +1,43 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerActions : MonoBehaviour
 {
     private Input inputPlayer;
+    [SerializeField] private float attackCooldown = 4f; // Tiempo de espera entre ataques
+    private bool isAttacking = false;
+
     void Start()
     {
         inputPlayer = new Input();
         inputPlayer.Player.Attack.Enable();
-        inputPlayer.Player.Attack.performed += ctx => Attack();
+        Activate();
     }
 
-    private void Attack()
+    private void Activate()
     {
-        Debug.Log("Attack");
+        inputPlayer.Player.Attack.performed += Attack;
+    }
+
+    private void Deactivate()
+    {
+        inputPlayer.Player.Attack.performed -= Attack;
+    }
+
+    private void Attack(InputAction.CallbackContext ctx)
+    {
+        if (isAttacking) return; // Evitar ataques mientras ya se está atacando
+        isAttacking = true;
+        print("Attack");
+        StartCoroutine(ColdDownAttack());
+    }
+
+    private IEnumerator ColdDownAttack()
+    {
+        yield return null;
+        yield return new WaitForSeconds(attackCooldown);
+        isAttacking = false;
     }
 
     void OnDisable()
